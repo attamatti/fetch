8f2b6f167570556cf9880bfcd4242b05b7e0488f
Matthew Iadanza <fbsmi@absl-wkstn01.leeds.ac.uk>
2019-05-16 15:48:40 +0100

    added stats visulization

---
+++ fetch_stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# do stats in teh DL.log fine from fetch
+
+import matplotlib.pyplot as plt
+
+data = open('DL.log','r').readlines()
+
+dates = {}
+scripts = {}
+users = {}
+
+for i in data:
+    (user,date,script) = (i.split()[0],''.join(i.split()[1].split('-')[0:2]),i.split()[3].split('/')[-1])
+    try:
+        dates[date] +=1
+    except:
+        dates[date] = 1
+    try:
+        users[user] +=1
+    except:
+        users[user] = 1
+    try:
+        scripts[script] +=1
+    except:
+        scripts[script] = 1
+
+def makeplot(dict,name):
+    dkeys = dict.keys()
+    dkeys.sort()
+    vals = [dict[x] for x in dkeys]
+    x= range(len(vals))
+    plt.bar(x,vals,align='center')
+    plt.xticks(x,dkeys,rotation='vertical',fontsize =10,horizontalalignment='center')
+    plt.ylabel('downloads')
+    plt.xlim(-1,len(vals))
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('{0}.png'.format(name))
+    plt.close()
+
+makeplot(scripts,'scripts')    
+makeplot(dates,'dates')    
+makeplot(users,'users')    
+
+    
